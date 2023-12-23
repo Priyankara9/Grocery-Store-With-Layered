5ffca229c6057caa8ed26ab6cb122d762dd9c4b8
Priyankara9 <cemahesh92@gmail.com>
2023-12-23 06:52:56 -0800

    set onAction to customerButton in DashBoardForm to loadcustomerForm

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,14 +1,26 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashBoardController {
     public AnchorPane pane;
     public Label lblTime;
 
     public void customerButtonOnAction(ActionEvent actionEvent) {
+        Stage stage= (Stage) pane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/CustomerForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void itemsButtonOnAction(ActionEvent actionEvent) {
