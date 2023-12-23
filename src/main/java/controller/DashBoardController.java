367ca3bc11f7964f20dcb868ce9914ffdb5b0794
Priyankara9 <cemahesh92@gmail.com>
2023-12-23 08:17:29 -0800

    set real Date & time on DashBoardForm

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,17 +1,35 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashBoardController {
     public AnchorPane pane;
     public Label lblTime;
+    public void initialize(){
+        setTimeOnScreen();
+
+    }
+
+    private void setTimeOnScreen() {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO,e->
+                lblTime.setText(LocalDateTime.now().format(DateTimeFormatter
+                        .ofPattern("yyyy-MM-dd  HH:mm:ss")))),new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
 
     public void customerButtonOnAction(ActionEvent actionEvent) {
         Stage stage= (Stage) pane.getScene().getWindow();
