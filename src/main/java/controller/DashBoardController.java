8e5f6e75b1fc56cf71f16d5ed2d0d99006ae1416
Priyankara9 <cemahesh92@gmail.com>
2023-12-23 03:39:57 -0800

    create customer button,item button and place order button

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class DashBoardController {
+    public AnchorPane pane;
+    public Label lblTime;
+
+    public void customerButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void itemsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void placeOrderButtonOnAction(ActionEvent actionEvent) {
+    }
 }
